b41a1bd1d68f2010a917d90b6e5503267296bc38
Stik <stikdragon@gmail.com>
2022-02-11 17:11:49 +0000

    made modular

---
+++ src/module-info.java
@@ -0,0 +1,14 @@
+module uk.co.stikman.stikutils {
+	requires org.json;
+	requires junit;
+	
+	exports uk.co.stikman.csv;
+	exports uk.co.stikman.eventbus;
+	exports uk.co.stikman.jsonwrap;
+	exports uk.co.stikman.log;
+	exports uk.co.stikman.table;
+	exports uk.co.stikman.users;
+	exports uk.co.stikman.utils;
+	exports uk.co.stikman.utils.math;
+	exports uk.co.stikman.utils.textsvr;
+}
